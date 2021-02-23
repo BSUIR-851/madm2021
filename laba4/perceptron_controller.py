eaf4f2105846909038219985162928e2b6f2cc0c
yuramorozov01 <yura_morozov01@mail.ru>
2021-02-23 22:53:14 +0300

    feat: add vector classification

--- laba4/perceptron_controller.py
+++ laba4/perceptron_controller.py
@@ -21,7 +21,22 @@ class PerceptronController(QtCore.QObject):
 		self.ui.setupUi(self.form)
 
 	def start(self):
+		self.__perceptron = None
+		self.__amount_of_classes = 3
+		self.__amount_of_vectors = 4
+		self.__vector_size = 6
+
+		self.ui.le_amount_of_classes.setPlaceholderText(str(self.__amount_of_classes))
+		self.ui.le_amount_of_classes.setText(str(self.__amount_of_classes))
+
+		self.ui.le_amount_of_vectors.setPlaceholderText(str(self.__amount_of_vectors))
+		self.ui.le_amount_of_vectors.setText(str(self.__amount_of_vectors))
+		
+		self.ui.le_vector_size.setPlaceholderText(str(self.__vector_size))
+		self.ui.le_vector_size.setText(str(self.__vector_size))
+
 		self.ui.pb_train.clicked.connect(self.__pb_train_click)
+		self.ui.pb_classificate.clicked.connect(self.__pb_classificate_click)
 
 		self.__update_form()
 		sys.exit(self.app.exec_())
@@ -40,26 +55,26 @@ class PerceptronController(QtCore.QObject):
 
 	def __pb_train_click(self):
 		try:
-			amount_of_classes = int(self.ui.le_amount_of_classes.text())
+			self.__amount_of_classes = int(self.ui.le_amount_of_classes.text())
 		except ValueError:
-			amount_of_classes = 3
+			self.ui.le_amount_of_classes.setText(str(self.__amount_of_classes))
 
 		try:
-			amount_of_vectors = int(self.ui.le_amount_of_vectors.text())
+			self.__amount_of_vectors = int(self.ui.le_amount_of_vectors.text())
 		except ValueError:
-			amount_of_vectors = 4
+			self.ui.le_amount_of_vectors.setText(str(self.__amount_of_vectors))
 
 		try:
-			vector_size = int(self.ui.le_vector_size.text())
+			self.__vector_size = int(self.ui.le_vector_size.text())
 		except ValueError:
-			vector_size = 6
+			self.ui.le_vector_size.setText(str(self.__vector_size))
 
-		training_vectors = self.__generate_training_vectors(amount_of_classes, amount_of_vectors, vector_size)
+		training_vectors = self.__generate_training_vectors(self.__amount_of_classes, self.__amount_of_vectors, self.__vector_size)
 
-		perceptron = Perceptron(amount_of_classes, vector_size)
-		funcs, isSuccess = perceptron.train(training_vectors)
+		self.__perceptron = Perceptron(self.__amount_of_classes, self.__vector_size)
+		funcs, isSuccess = self.__perceptron.train(training_vectors)
 		if not isSuccess:
-			self.__msgbox_message('Error', 'An error occurred while classification')
+			self.__msgbox_message('Error', 'An error occurred while classification. \nTry again or enter another input data.')
 		else:
 			self.__fill_result_funcs(funcs)
 
@@ -88,6 +103,31 @@ class PerceptronController(QtCore.QObject):
 				func_str += '| {:^7} |'.format(coeff)
 			self.ui.pte_functions.appendPlainText(func_str)
 		self.__update_form()
+
+	def __pb_classificate_click(self):
+		try:
+			if not self.__perceptron:
+				raise AttributeError('Do a train first')
+
+			vector_str = self.ui.le_vector.text().strip()
+			vector = [int(num_str) for num_str in vector_str.split(' ')]
+			
+			if len(vector) != self.__vector_size:
+				raise ValueError('Size of vector must be {}'.format(self.__vector_size))
+
+			vector.append(1)
+			try:
+				max_class = self.__perceptron.get_max_function_result(np.array(vector))
+				self.ui.le_result.setText('Class: {}'.format(max_class))
+			except ValueError as ve:
+				self.__msgbox_message('Error', str(ve))
+
+		except ValueError as ve:
+			self.__msgbox_message('Error', str(ve))
+
+		except AttributeError as ae:
+			self.__msgbox_message('Error', str(ae))
+
 
 if __name__ == '__main__':
 	perceptron_controller = PerceptronController()
