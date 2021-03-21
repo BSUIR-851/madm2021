fcee15a41d1b21c423fc81bc37d4e170b99ab19d
yuramorozov01 <yura_morozov01@mail.ru>
2021-03-21 18:52:14 +0300

    feat: add class for storing image info (name, path, pixmap)

---
+++ laba7/syntactic.py
@@ -0,0 +1,28 @@
+from PyQt5.QtGui import QPixmap
+
+
+class SyntacticPicture():
+	def __init__(self, name, file_path):
+		self.__name = name
+		self.__file_path = file_path
+		self.__pixmap = QPixmap(self.__file_path)
+
+	@property
+	def pixmap(self):
+		return self.__pixmap
+	
+	@property
+	def name(self):
+		return self.__name
+	
+	@name.setter
+	def name(self, value):
+		self.__name = value
+
+	@property
+	def file_path(self):
+		return self.__file_path
+	
+	@file_path.setter
+	def file_path(self, value):
+		self.__file_path = value
