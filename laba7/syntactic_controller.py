bab8ec700e847f2f50acfa1f082dba1162ae1dda
yuramorozov01 <yura_morozov01@mail.ru>
2021-03-23 20:32:34 +0300

    feat: add grammar analyzing

--- laba7/syntactic_controller.py
+++ laba7/syntactic_controller.py
@@ -13,14 +13,18 @@ from point import Point
 class ImageScene(QtWidgets.QGraphicsScene):
 	click_signal = QtCore.pyqtSignal(float, float)
 
-	def __init__(self):
+	def __init__(self, base_image_path):
 		super().__init__()
+		self.__base_image_pixmap = QtGui.QPixmap(base_image_path)
 
 	# overriding mousePressEvent
 	def mousePressEvent(self, event):
 		cursor_x, cursor_y = event.scenePos().x(), event.scenePos().y()
 		self.click_signal.emit(cursor_x, cursor_y)
 
+	def drawBackground(self, painter, rect):
+		painter.setOpacity(0.1)
+		painter.drawPixmap(int(self.width() / 2 - self.__base_image_pixmap.width() / 2), int(self.height() / 2 - self.__base_image_pixmap.height() / 2), self.__base_image_pixmap)
 
 class SyntacticController(QtCore.QObject):
 	def __init__(self):
@@ -40,7 +44,7 @@ class SyntacticController(QtCore.QObject):
 		self.__canvas_height = 600
 		self.ui.gv_canvas.setFixedSize(self.__canvas_width, self.__canvas_height)
 
-		self.__scene = ImageScene()
+		self.__scene = ImageScene('./dog.png')
 		self.__scene.click_signal.connect(self.__click_handler)
 		self.__scene.setSceneRect(0, 0, self.__canvas_width, self.__canvas_height)
 
@@ -51,7 +55,9 @@ class SyntacticController(QtCore.QObject):
 		base_path = './images'
 		search_extension = '.png'
 		try:
-			for file in [f for f in os.listdir(base_path) if f.endswith(search_extension)]:
+			image_list = os.listdir(base_path)
+			image_list.sort()
+			for file in [f for f in image_list if f.endswith(search_extension)]:
 				file_name = os.path.splitext(file)[0]
 				self.__images.append(SyntacticImage(file_name, '{}/{}'.format(base_path, file)))
 		except FileNotFoundError:
@@ -73,7 +79,7 @@ class SyntacticController(QtCore.QObject):
 			self.__scene.addItem(pixmap_item)
 
 			self.__start_positions[image.name] = Point(x - offset_x, y - offset_y)
-			self.__end_positions[image.name] = Point(x - offset_x + offset_x * 2, y - offset_y + offset_y * 2)
+			self.__end_positions[image.name] = Point(x + offset_x, y + offset_y)
 
 			self.ui.cb_primitives.removeItem(current_index)
 
@@ -119,10 +125,101 @@ class SyntacticController(QtCore.QObject):
 		self.__init_images()
 		self.__fill_list_of_primitives()
 
+	def __get_grammar(self):
+		grammar = {
+			'body': {
+				'parent': {
+					'name': None,
+					'start_x': 50,
+					'end_x': 750,
+					'start_y': 50,
+					'end_y': 550,
+				},
+				'start_x': self.__start_positions['body'].x,
+				'end_x': self.__end_positions['body'].x,
+				'start_y': self.__start_positions['body'].y,
+				'end_y': self.__end_positions['body'].y,
+			},
+
+			'ear': {
+				'parent': {
+					'name': 'body',
+					'start_x': self.__start_positions['body'].x + 115,
+					'end_x': self.__start_positions['body'].x + 190,
+					'start_y': self.__start_positions['body'].y,
+					'end_y': self.__start_positions['body'].y + 129,
+				},
+				'start_x': self.__start_positions['ear'].x,
+				'end_x': self.__end_positions['ear'].x,
+				'start_y': self.__start_positions['ear'].y,
+				'end_y': self.__end_positions['ear'].y,
+			},
+
+			'eye': {
+				'parent': {
+					'name': 'body',
+					'start_x': self.__start_positions['body'].x + 180,
+					'end_x': self.__start_positions['body'].x + 207,
+					'start_y': self.__start_positions['body'].y + 34,
+					'end_y': self.__start_positions['body'].y + 55,
+				},
+				'start_x': self.__start_positions['eye'].x,
+				'end_x': self.__end_positions['eye'].x,
+				'start_y': self.__start_positions['eye'].y,
+				'end_y': self.__end_positions['eye'].y,
+			},
+
+			'front_left_paw': {
+				'parent': {
+					'name': 'body',
+					'start_x': self.__start_positions['body'].x + 175,
+					'end_x': self.__start_positions['body'].x + 245,
+					'start_y': self.__start_positions['body'].y + 103,
+					'end_y': self.__start_positions['body'].y + 170,
+				},
+				'start_x': self.__start_positions['front_left_paw'].x,
+				'end_x': self.__end_positions['front_left_paw'].x,
+				'start_y': self.__start_positions['front_left_paw'].y,
+				'end_y': self.__end_positions['front_left_paw'].y,
+			},
+
+			'hind_left_paw': {
+				'parent': {
+					'name': 'body',
+					'start_x': self.__start_positions['body'].x + 55,
+					'end_x': self.__start_positions['body'].x + 95,
+					'start_y': self.__start_positions['body'].y + 150,
+					'end_y': self.__start_positions['body'].y + 195,
+				},
+				'start_x': self.__start_positions['hind_left_paw'].x,
+				'end_x': self.__end_positions['hind_left_paw'].x,
+				'start_y': self.__start_positions['hind_left_paw'].y,
+				'end_y': self.__end_positions['hind_left_paw'].y,
+			},
+		}
+		return grammar
+
+	def __analize_grammar_item(self, item):
+		isCorrect = False
+		if (item.get('start_x') > item.get('parent').get('start_x')) and (item.get('end_x') < item.get('parent').get('end_x')) and (item.get('start_y') > item.get('parent').get('start_y')) and (item.get('end_y') < item.get('parent').get('end_y')):
+			isCorrect = True
+		return isCorrect
+
 	def __pb_recognize_click(self):
-		pass
-
-
+		if self.__images:
+			self.__msgbox_message('Recognition error', 'Put all items on canvas')
+		else:
+			isCorrect = True
+			grammar = self.__get_grammar()
+			for grammar_item in grammar.keys():
+				if not self.__analize_grammar_item(grammar.get(grammar_item)):
+					isCorrect = False
+					self.__msgbox_message('Recognition error', 'Offset of {} is too big'.format(grammar_item))
+					break
+			
+			if isCorrect:
+				self.__msgbox_message('Recognition', 'Object was recognized')
+	
 if __name__ == '__main__':
 	syntactic_controller = SyntacticController()
 	syntactic_controller.start()
